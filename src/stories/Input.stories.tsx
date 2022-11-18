ccab6c26998bb9d7a891c787a9939db708c75b7f
artem_ <artem_5511@mail.ru>
2022-11-19 00:53:38 +0300

    rendering

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 
 export default {
   title: 'input',
@@ -7,4 +7,25 @@ export default {
 
 export const UncontrolledInput = () => <input/>;
 
-export const ControlledInput = () => <input value={'IT-Incubator'}/>;
+export const TrackValueOfControlledInput = () => {
+  const [value, setValue] = useState('');
+
+  return <><input onChange={(event) =>{
+  const actualValue = event.currentTarget.value;
+  setValue(actualValue);
+  } } /> - {value}</>
+}
+
+export const GetValueOfUncontrolledInputByButtonPress = () => {
+  const [value, setValue] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const save = () => {
+    const el = inputRef.current as HTMLInputElement;
+    setValue(el.value);
+  }
+
+  return <><input ref={inputRef} id={'inputId'}/> <button onClick={save}>save</button> - actual value: {value}</>
+};
+
+export const ControlledInputWithFixedValue = () => <input value={'it-incubator.by'}/>
