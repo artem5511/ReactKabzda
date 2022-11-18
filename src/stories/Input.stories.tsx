183a21af91fbc2cdb583bc3503e183d3317b56ae
artem_ <artem_5511@mail.ru>
2022-11-19 00:33:52 +0300

    rendering

---
+++ src/stories/Input.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default {
+  title: 'input',
+  // component: input
+  }
+
+export const UncontrolledInput = () => <input/>;
+
+export const ControlledInput = () => <input value={'IT-Incubator'}/>;
